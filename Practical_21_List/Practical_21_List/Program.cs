dad0280b685bc9c22049bd9a3f6abb582084829b
Meet Pandya <128979370+meet2318@users.noreply.github.com>
2023-06-26 17:24:29 +0530

    Add files via upload

---
+++ Practical_21_List/Practical_21_List/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+/// <summary>
+///   <br />
+/// </summary>
+class Program
+{
+    /// <summary>Defines the entry point of the application.</summary>
+    /// <param name="args">The arguments.</param>
+    public static void Main(string[] args)
+    {
+        IList<string> meet = new List<string> { "A", "B", "C", "D", "A", "C", "E", "E", "E" };
+        string joined = string.Join(",", meet);
+        Console.WriteLine(joined);
+        // Removing duplicate elements
+        List<string> distinct = meet.Distinct().ToList();
+        Console.WriteLine("List after removing duplicate elements ...");
+        foreach (string res in distinct)
+        {
+            Console.WriteLine("{0}", res);
+        }
+    }
+}
---
+++ Practical_22_File_Handling/Practical_22_File_Handling/Practical_22_File_Handling/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+/// <summary>
+///   <br />
+/// </summary>
+class fileHandling
+{
+    /// <summary>Defines the entry point of the application.</summary>
+    public static void Main()
+    {
+        string fileName = @"F:\Assignment_Programs\Practical_22_File_Handling\myTestFile.txt";
+
+        try
+        {
+            // Delete the file if it exists.
+            if (File.Exists(fileName))
+            {
+                File.Delete(fileName);
+            }
+            Console.Write("\n\nCreate a file with text and read the file:\n");
+            Console.Write("----------------------------------------------\n");
+            // Create the file.
+            using (StreamWriter fileStr = File.CreateText(fileName))
+            {
+                fileStr.WriteLine("\nName: Meet Pandya");
+                fileStr.WriteLine("Education: MSC(IT & CA)");
+                fileStr.WriteLine("Age:23");
+                fileStr.WriteLine("Home Town: Porbandar");
+            }
+            using (StreamReader sr = File.OpenText(fileName))
+            {
+                string s = "";
+                Console.WriteLine("Here is the content of the file myTestFile.txt: ");
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine("");
+            }
+        }
+        catch (Exception MyExcep)
+        {
+            Console.WriteLine(MyExcep.ToString());
+        }
+    }
+}

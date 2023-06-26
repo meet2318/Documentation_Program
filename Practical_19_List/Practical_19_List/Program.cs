7212e631093d2e07756f60fd166b9f9ed1fcbd9d
Meet Pandya <128979370+meet2318@users.noreply.github.com>
2023-06-26 17:22:26 +0530

    Add files via upload

---
+++ Practical_19_List/Practical_19_List/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// 
+/// </summary>
+namespace Practical_19_List
+{
+    /// <summary>
+    ///   <br />
+    /// </summary>
+    class Program
+    {
+        /// <summary>Defines the entry point of the application.</summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Your Name:");
+            string userName = Console.ReadLine();
+            IList<string> strings = new List<string> { "Hello", "World", userName };
+            string joined = string.Join(",", strings);
+            Console.WriteLine(joined);
+        }
+    }
+}
---
+++ Practical_20_Dictionary/Practical_20_Dictionary/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+public class Demo
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter Your Name:");
+        string userName = Console.ReadLine();
+        Dictionary<string, string> dict = new Dictionary<string, string>();
+        Dictionary<string, string> dict1 = new Dictionary<string, string>();
+        dict.Add("Name", userName);
+        Console.WriteLine("\n");
+        dict.Add("Education", "MSC(IT & CA)");
+        dict.Add("Age", "23");
+        dict.Add("HomeTown", "Porbandar");
+        foreach (var item in dict)
+        {
+            Console.WriteLine("Key:- " + item.Key + " Value:- "+ item.Value);
+        }
+    }
+}

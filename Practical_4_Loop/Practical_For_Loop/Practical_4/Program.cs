1c861944caf1af63fcb8623b29aa57c7b58490c2
Meet Pandya <128979370+meet2318@users.noreply.github.com>
2023-06-26 18:41:08 +0530

    Add files via upload

---
+++ Practical_4_Loop/Practical_For_Loop/Practical_4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+/// <summary>
+///   <br />
+/// </summary>
+class Program
+{
+    /// <summary>Defines the entry point of the application.</summary>
+    /// <param name="args">The arguments.</param>
+    public static void Main(string[] args)
+    {
+        for(int i=1;i<=100;i++)
+        {
+            Console.WriteLine(i);
+        }
+        Console.ReadKey();
+    }
+}

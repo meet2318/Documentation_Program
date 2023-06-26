47effd234e07cc8c4bd0e6e9afa61e5c5905cd94
Meet Pandya <128979370+meet2318@users.noreply.github.com>
2023-06-26 15:46:41 +0530

    Add files via upload

---
+++ Practical_7_Constructors/Practical_7_Constructors/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+/// <summary>
+///   <br />
+/// </summary>
+
+namespace Practical_7_Constructors
+{
+
+    public class Program
+    {
+        /// <summary>Defines the entry point of the application.</summary>
+        /// <param name="args">The arguments.</param>
+        public static void Main(string[] args)
+        {
+            int a, b;
+
+            Console.Write("Enter two numbers:\n");
+            a = Convert.ToInt32(Console.ReadLine());
+            b = Convert.ToInt32(Console.ReadLine());
+
+            Test t1 = new Test();
+
+            Test t2 = new Test(a, b);
+            t2.display();
+        }
+    }
+}
---
+++ Practical_7_Constructors/Practical_7_Constructors/Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_7_Constructors
+{
+    public class Test
+    {
+        /// <summary>The s</summary>
+        int s;
+
+        /// <summary>Initializes a new instance of the <see cref="Test" /> class.</summary>
+        public Test()
+        {
+            Console.Write("Sum is:");
+        }
+        /// <summary>Initializes a new instance of the <see cref="Test" /> class.</summary>
+        /// <param name="a">a.</param>
+        /// <param name="b">The b.</param>
+        public Test(int a, int b)
+        {
+            s = a + b;
+        }
+
+        /// <summary>Displays this instance.</summary>
+        public void display()
+        {
+            Console.WriteLine(s);
+        }
+
+
+    }
+}

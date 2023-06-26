45d6b8c08fe8794eafd4e3a175b2b53bf405a777
Meet Pandya <128979370+meet2318@users.noreply.github.com>
2023-06-26 16:11:42 +0530

    Add files via upload

---
+++ Practical_9_Wrapper_Class/Practical_9_ Wrapper_Classes/Practical_9_ Wrapper_Classes/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+/// <summary>
+///   <br />
+/// </summary>
+public class StringExample
+{
+    /// <summary>Defines the entry point of the application.</summary>
+    /// <param name="args">The arguments.</param>
+    public static void Main(string[] args)
+    {
+        // Example Of Split
+        string s1 = "Hello C Sharp";
+        string[] s2 = s1.Split(' ');
+        foreach (string s4 in s2)
+        {
+            Console.WriteLine(s4);
+        }
+        Console.WriteLine("--------------------------------------------------\n");
+        // Example Of Upper 
+        string s3 = s1.ToUpper();
+        Console.WriteLine(s3);
+        Console.WriteLine("--------------------------------------------------\n");
+        // Example Of Concat
+        string s11 = "Hello ";
+        string s12 = "C#";
+        Console.WriteLine(string.Concat(s11, s12));
+        Console.WriteLine("--------------------------------------------------\n");
+        // Example Of Equals
+        Console.WriteLine(s1.Equals(s11));
+        Console.WriteLine(s1.Equals(s12));
+        Console.WriteLine("--------------------------------------------------\n");
+
+    }
+}

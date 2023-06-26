bdb4cd6f2bb8d90a53786691f28b309d0fa714b7
Meet Pandya <128979370+meet2318@users.noreply.github.com>
2023-06-26 16:40:01 +0530

    Add files via upload

---
+++ Practical_13_Encapsulation/Practical_Of_Encapsulation/Program.cs
@@ -0,0 +1,25 @@
+﻿using Practical_Of_Encapsulation;
+using System;
+namespace AccessSpecifiers
+{
+    /// <summary>
+    ///   <br />
+    /// </summary>
+    class Program
+    {
+        /// <summary>Defines the entry point of the application.</summary>
+        /// <param name="args">The arguments.</param>
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            // Setting values to the properties  
+            student.ID = "101";
+            student.Name = "Meet Pandya";
+            student.Email = "meet@example.com";
+            // getting values  
+            Console.WriteLine("ID = " + student.ID);
+            Console.WriteLine("Name = " + student.Name);
+            Console.WriteLine("Email = " + student.Email);
+        }
+    }
+}
---
+++ Practical_13_Encapsulation/Practical_Of_Encapsulation/Student.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// 
+/// </summary>
+namespace Practical_Of_Encapsulation
+{
+    /// <summary>
+    ///   <br />
+    /// </summary>
+    internal class Student
+    {
+        // Creating setter and getter for each property  
+        /// <summary>Gets or sets the identifier.</summary>
+        /// <value>The identifier.</value>
+        public string ID { get; set; }
+        /// <summary>Gets or sets the name.</summary>
+        /// <value>The name.</value>
+        public string Name { get; set; }
+        /// <summary>Gets or sets the email.</summary>
+        /// <value>The email.</value>
+        public string Email { get; set; }
+    }
+}
